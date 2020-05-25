ced39fa13137231a099e6c51169fd1c5beb741f7
defep <federico.palma@hotmail.com>
2020-05-25 00:58:59 -0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,4 +1,6 @@
             <footer class="mt-5">
+                <h6><?php bloginfo( 'name' ); ?></h6>
+                <p>Created by <a href="https://github.com/defep" target="_blank">defep</a></p>
             </footer>
             </div>
         </div>
