a7cb41d048d64e2470c094c371a9d01a68539a16
defep <federico.palma@hotmail.com>
2020-05-25 01:26:01 -0300

    Update single.php

--- single.php
+++ single.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 <?php the_post(); ?>
 <div id="last-modified">Updated: <?php the_modified_time('F j, Y'); ?> </div>
-<h2><a class="covert-link" href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+<h2 class="mb-4"><a class="covert-link" href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
 <?php the_content(); ?>
 
 <div class="row pt-4">
