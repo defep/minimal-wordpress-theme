2d925049a01e79527df4f35ff793b0165769afcd
defep <federico.palma@hotmail.com>
2020-05-25 01:24:04 -0300

    Create single.php

---
+++ single.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+<?php the_post(); ?>
+<div id="last-modified">Updated: <?php the_modified_time('F j, Y'); ?> </div>
+<h2><a class="covert-link" href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+<?php the_content(); ?>
+
+<div class="row pt-4">
+    <div class="col">
+        <?php comments_template(); ?>
+    </div>
+</div>
+
+<?php get_footer(); ?>
